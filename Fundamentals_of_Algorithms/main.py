c9ec47a7b3edaa3f5f708a90c504e3ec87710dbe
islam0122 <duishobaevislam01@gmail.com>
2025-10-25 22:48:27 +0600

    lab_5-lab-6

--- Fundamentals_of_Algorithms/main.py
+++ Fundamentals_of_Algorithms/main.py
@@ -67,12 +67,12 @@
 # Пузырьковая сортировка (Bubble Sort)
 # def bubble_sort(arr):
 #     n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-            print(arr)
-    return arr
+#     for i in range(n):
+#         for j in range(0, n-i-1):
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+#             print(arr)
+#     return arr
 #
 # # Пример
 # numbers = [5, 2, 9, 1, 5]
